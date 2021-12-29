1b4d2ba648f15dd26e2f9b57ebbe61dd794ca9c9
Valentin <val1915@yandex.ru>
2021-12-29 22:07:48 +0300

    Thread#start()

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,17 @@
+package ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread second = new Thread(
+                ()-> System.out.println(Thread.currentThread().getName())
+        );
+        for(int i = 0; i <10; i++) {
+            Thread third = new Thread(
+                    ()-> System.out.println(Thread.currentThread().getName())
+            );
+            System.out.println(Thread.currentThread().getName());
+            third.start();
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+}
