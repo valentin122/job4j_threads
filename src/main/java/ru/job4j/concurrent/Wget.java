66592a21fe3b50b1d6a042d761299c4e2fca55fe
Valentin <val1915@yandex.ru>
2021-12-30 00:33:35 +0300

    Скачивание файла с ограничением. исправления

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,23 +1,49 @@
 package ru.job4j.concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        System.out.print("Start loading ... ");
-                        for (int i = 0; i <= 100; i++) {
-                            System.out.print("\rLoading : " + i  + "%");
-                            Thread.sleep(100);
-                        }
-                        System.out.println();
-                        System.out.println("Loaded.");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-        );
-        thread.start();
-        System.out.println("Main");
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[speed];
+            int bytesRead;
+            Date startDate = new Date();
+            System.out.println("Start downloading: " + startDate.toString());
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                long startTime = new Date().getTime();
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long timeLimit = 1000 - (new Date().getTime() - startTime);
+                System.out.println("timeLimit: " + timeLimit);
+                Thread.sleep(timeLimit);
+            }
+            System.out.println("End downloading: " + new Date().toString());
+            System.out.println("Time expend: " + (new Date().getTime() - startDate.getTime()) + "ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        int speed = 1024;
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
