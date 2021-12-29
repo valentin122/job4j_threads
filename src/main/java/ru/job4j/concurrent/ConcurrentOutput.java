f263783049b083d18c8919bd229ed96058d14bc1
Valentin <val1915@yandex.ru>
2021-12-29 22:23:41 +0300

    ...

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -2,7 +2,7 @@ package ru.job4j.concurrent;
 
 public class ConcurrentOutput {
     public static void main(String[] args) {
-        Thread second = new Thread(
+        Thread first = new Thread(
                 ()-> System.out.println(Thread.currentThread().getName())
         );
         for(int i = 0; i <10; i++) {
