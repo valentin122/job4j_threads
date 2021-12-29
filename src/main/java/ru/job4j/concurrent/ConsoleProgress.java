965b03ab640f3c05e9277122e378b6033928cff7
Valentin <val1915@yandex.ru>
2021-12-30 00:27:29 +0300

    Прерывание нити

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -12,7 +12,7 @@ public class ConsoleProgress implements Runnable {
         while (!Thread.currentThread().interrupted()) {
             try {
                for (String character : process) {
-                Thread.sleep(220);
+                Thread.sleep(221);
                     System.out.print("\r load: " + character);
                }
             } catch (InterruptedException e) {
