da18d9681530e48e24fc0863d87047a2f70c2c7c
Valentin <val1915@yandex.ru>
2022-01-01 17:22:43 +0300

    исправления.

--- src/main/java/ru/job4j/concurrent/Cache.java
+++ src/main/java/ru/job4j/concurrent/Cache.java
@@ -1,8 +1,7 @@
 package ru.job4j.concurrent;
 
 public final class Cache {
-    private static volatile Cache cache;
-    private static final Object LOCK = new Object();
+    private static Cache cache;
 
     public static synchronized Cache instOf() {
         if (cache == null) {
