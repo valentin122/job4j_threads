8898c5b18626fee08acffb0bb7126eace8f48066
Valentin <val1915@yandex.ru>
2022-03-15 22:46:37 +0300

    Реализовать шаблон Producer Consumer. v5

--- src/main/java/ru/job4j/concurrent/ParallelSearch.java
+++ src/main/java/ru/job4j/concurrent/ParallelSearch.java
@@ -5,40 +5,36 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class ParallelSearch {
 
     public static void main(String[] args) throws InterruptedException {
-        AtomicBoolean flag = new AtomicBoolean(Boolean.TRUE);
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(1);
         final Thread consumer = new Thread(
                 () -> {
-                    while (flag.get()) {
+                    while (!Thread.currentThread().isInterrupted()) {
                         try {
                             System.out.println(queue.poll());
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
                         }
                     }
                     System.out.println("consumer end");
-                    Thread.currentThread().interrupt();
                 }
         );
         final Thread producer = new Thread(
                 () -> {
-                    for (int index = 0; index != 5; index++) {
+                    for (int index = 0; index != 3; index++) {
                         try {
                             queue.offer(index);
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
                         }
                         try {
                             Thread.sleep(500);
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
                         }
                     }
-                    flag.set(Boolean.FALSE);
                     System.out.println("search end");
-                    Thread.currentThread().interrupt();
+                    consumer.interrupt();
                 }
-
         );
         consumer.start();
         producer.start();
