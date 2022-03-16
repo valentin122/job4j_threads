eaca85f84442447c255fbc42a460c4748cd47a33
Valentin <val1915@yandex.ru>
2022-03-16 21:57:50 +0300

    Реализовать шаблон Producer Consumer. v6

--- src/main/java/ru/job4j/concurrent/ParallelSearch.java
+++ src/main/java/ru/job4j/concurrent/ParallelSearch.java
@@ -1,44 +1,39 @@
 package ru.job4j.concurrent;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 public class ParallelSearch {
 
-    public static void main(String[] args) throws InterruptedException {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(1);
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(1);
         final Thread consumer = new Thread(
                 () -> {
                     while (!Thread.currentThread().isInterrupted()) {
                         try {
                             System.out.println(queue.poll());
                         } catch (InterruptedException e) {
+                            e.printStackTrace();
                             Thread.currentThread().interrupt();
                         }
                     }
-                    System.out.println("consumer end");
                 }
         );
-        final Thread producer = new Thread(
+        consumer.start();
+        new Thread(
                 () -> {
                     for (int index = 0; index != 3; index++) {
                         try {
                             queue.offer(index);
                         } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
+                            e.printStackTrace();
                         }
                         try {
                             Thread.sleep(500);
                         } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
+                            e.printStackTrace();
                         }
                     }
-                    System.out.println("search end");
                     consumer.interrupt();
                 }
-        );
-        consumer.start();
-        producer.start();
-        consumer.join();
-        System.out.println("all end");
+        ).start();
     }
+
 }
