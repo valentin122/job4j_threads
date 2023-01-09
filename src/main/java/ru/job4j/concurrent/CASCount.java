9a5d3d2b82b96f04d14624d8267e0be12bb6b168
Valentin <val1915@yandex.ru>
2023-01-09 20:04:06 +0300

    CAS - операции. ref.

--- src/main/java/ru/job4j/concurrent/CASCount.java
+++ src/main/java/ru/job4j/concurrent/CASCount.java
@@ -9,23 +9,15 @@ public class CASCount {
     private final AtomicReference<Integer> count = new AtomicReference<>(0);
 
     public void increment() {
-        Integer oldValue, newValue;
+        Integer oldValue;
+        Integer newValue;
         do {
             oldValue = count.get();
-            /*if (oldValue == null) {
-                oldValue = 0;
-            }*/
             newValue = oldValue + 1;
         } while (!count.compareAndSet(oldValue, newValue));
-
-        //throw new UnsupportedOperationException("Count is not impl.");
     }
 
     public int get() {
-        Integer result = count.get();
-        if (result == null) {
-            throw new UnsupportedOperationException("smth go wrong...");
-        }
-        return result;
+        return count.get();
     }
 }
