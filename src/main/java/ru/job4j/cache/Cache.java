de80afeb439f893280a5c0dd4c9c989a577c38e3
Valentin <val1915@yandex.ru>
2023-09-05 21:25:45 +0300

    Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,36 @@
+package ru.job4j.cache;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
+
+public class Cache {
+    private final ConcurrentHashMap<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        BiFunction<Integer, Base, Base> biFunc = (keyCache, modelCache) -> {
+            Base stored = memory.get(model.getId());
+            if (stored.getVersion() != model.getVersion()) {
+                throw new OptimisticException("Versions are not equal");
+            }
+            Base base = new Base(model.getId(), model.getVersion() + 1);
+            base.setName(model.getName());
+            return base;
+        };
+        return memory.computeIfPresent(model.getId(), biFunc) != null;
+    }
+
+    public void delete(Base model) {
+        memory.remove(model.getId());
+    }
+
+    public Map<Integer, Base> getMemory() {
+        return new HashMap<>(memory);
+    }
+}
